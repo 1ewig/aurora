31946e06962bc9155d24a9ad6ce2a2abbf158642
Moshu <asadshahid234@gmail.com>
2026-07-12 21:25:24 +0500

    config: add 80 to next.config.ts image qualities

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   cacheComponents: true,
   allowedDevOrigins: ['harmonics-paternity-blighted.ngrok-free.dev'],
   images: {
-    qualities: [100, 85],
+    qualities: [100, 85, 80],
     remotePatterns: [
       {
         protocol: 'https',
